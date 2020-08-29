fb77661d40c5dc8e7889b7f7685705fc7c2b6b10
Ömer Doğan <omer.dogan@kariyerits-MacBook-Pro-2.local>
2020-08-29 15:04:26 +0300

    add and remove address

--- cypress/integration/mtrendyol/addresses.test.js
+++ cypress/integration/mtrendyol/addresses.test.js
@@ -1,38 +1,15 @@
-describe("Login and Address Section", () => {
+describe("Login, add and delete address", () => {
+
+  before(() => {
+    cy.clearCookies()
+    cy.signIn({ email: "akagamina24@gmail.com", password: "maLjW@5rwFz?F3P" })
+  })
 
   beforeEach(() => {
     Cypress.Cookies.preserveOnce('_TRENDYOL_MBL_Auth')
   })
 
-  it("succesfully loads", () => {
-    cy.visit("/")
-  })
-
-  it("protocol should be https", () => {
-    cy.location("protocol").should("eq", "https:")
-  })
-
-  it("should be go to login page", () => {
-    cy.get("#header .header__icon-wrapper .header__icon:first a").click()
-  })
-
-  it("sign in tabs should be have active class", () => {
-    cy.get("#login-register .q-layout.header .header-buttons button").first().should("have.class", "active")
-  })
-
-  it("wrong email address or password should be return error message", () => {
-    cy.get(".q-layout.login .email-input input[type='email']").type("asdasdqwe@gmail.com")
-    cy.get(".q-layout.login .password-wrapper .password input[type='password']").type("asasdasdm")
-    cy.get("#login-register .q-layout.login button").should('be.visible').click()
-    cy.get("#login-register #error-box-wrapper").should("be.visible")
-  })
-
-  it("successfully login and go to address section", () => {
-    cy.get(".q-layout.login .password-wrapper .password input[type='password']").clear()
-    cy.get(".q-layout.login .email-input input[type='email']").clear()
-    cy.get(".q-layout.login .email-input input[type='email']").type("akagamina24@gmail.com")
-    cy.get(".q-layout.login .password-wrapper .password input[type='password']").type("maLjW@5rwFz?F3P")
-    cy.get("#login-register .q-layout.login button").should('be.visible').click()
+  it("go to address section", () => {
     cy.get("#header .header__icon-wrapper .header__icon:first a").click()
     cy.get("#account-menu .q-layout .q-box-group .q-box").within(() => {
       cy.get(".account-box-item .account-box-text").contains("Adres Bilgilerim").click()
@@ -44,7 +21,7 @@ describe("Login and Address Section", () => {
     cy.get("#address .q-modal-main").should("exist")
   })
 
-  it("address should be adding succesfully", () => {
+  it("address should be add succesfully", () => {
     cy.get(".q-modal-content .q-box .q-input-wrapper:first input[type='text']").click({ force: true }).type("Home")
     cy.get(".q-modal-content .q-box .grid-input-container .left input[type='text']").click({ force: true }).type("Omer")
     cy.get(".q-modal-content .q-box .grid-input-container .right input[type='text']").click({ force: true }).type("Dogan")
@@ -56,6 +33,11 @@ describe("Login and Address Section", () => {
     cy.get(".q-modal-content button").click()
     cy.get(".q-popup-main .address-creation-success").should("exist")
     cy.get(".q-popup-main .address-creation-success button").click()
+  })
+
+  it("address should be delete succesfully", () => {
+    cy.get(".address:first .q-badge > div").contains("Düzenle").click()
+    cy.get(".q-modal-main .q-modal-content button:last").click()
   })
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("signIn", (user) => {
+  cy.visit("/")
+  cy.location("protocol").should("eq", "https:")
+  cy.get("#header .header__icon-wrapper .header__icon:first a").click()
+  cy.get("form").within(() => {
+    cy.get("input[type='email']").type(user.email)
+    cy.get("input[type='password']").type(user.password)
+    cy.root().submit()
+  })
+})
